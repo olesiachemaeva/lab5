105f77565159587d62641d22d9e4dd9bf82759c3
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2021-11-28 14:59:33 +1000

    Добавьте файлы проекта.

---
+++ lab5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Ведите размер массива:");
+            int size = Convert.ToInt16(Console.ReadLine());
+            int [] array = new int [size];
+            for (int i=0; i < size; i++)
+            {
+                Console.WriteLine("введите  номер элемента: {0}", i + i);
+                array [i] = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.Clear();
+            double aa = 0;
+            foreach (int a in array) aa += Convert.ToDouble(a / size);
+            Console.WriteLine("Среднее арифмитическое значение: {0} ", aa);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ option/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace option
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] a = s.Split(' ');
+            double x = 0;
+            foreach (var i in a)
+            {
+                x += Convert.ToInt32(i);
+            }
+            double sr = x / a.Length;
+            Console.WriteLine(sr);
+        }
+    }
+}
